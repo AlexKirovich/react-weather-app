f47350bfa5e2b0661dbed07415af856c105d17ce
AlexKirovich <alex.k.kirovich@gmail.com>
2022-07-05 17:14:15 +0300

    Current weather info done

--- src/components/common/CurrentWeatherInfo/CurrentWeatherInfo.jsx
+++ src/components/common/CurrentWeatherInfo/CurrentWeatherInfo.jsx
@@ -1,7 +1,44 @@
-import s from "./CurrentWeatherInfo.scss";
+import s from "./CurrentWeatherInfo.module.scss";
+import Temp from "../../../assets/SVGs/Temp";
+import Humidity from "../../../assets/SVGs/Humidity";
+import Precipitation from "../../../assets/SVGs/Precipitation";
+import Wind from "../../../assets/SVGs/Wind";
 
 const CurrentWeatherInfo = () => {
-  return <div className={s.CurrentWeatherInfo}>jasn</div>;
+  return (
+    <div className={s.CurrentWeatherInfo}>
+      <div className={s.infoRows}>
+        <div className={s.infoContent}>
+          <div className={s.iconBkg}>
+            <Temp />
+          </div>
+          <p>Temperature</p>
+          <p>20° - feels like 17°</p>
+        </div>
+        <div className={s.infoContent}>
+          <div className={s.iconBkg}>
+            <Humidity/>
+          </div>
+          <p>Humidity</p>
+          <p>58% - the dew point is 14%</p>
+        </div>
+        <div className={s.infoContent}>
+          <div className={s.iconBkg}>
+            <Precipitation />
+          </div>
+          <p>Precipitation</p>
+          <p>No precipitation</p>
+        </div>
+        <div className={s.infoContent}>
+          <div className={s.iconBkg}>
+            <Wind />
+          </div>
+          <p>Wind</p>
+          <p>16 kilometers per hour</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CurrentWeatherInfo;
