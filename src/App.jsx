02ba3988658e21366ea4500153dd3934cca53587
AlexKirovich <alex.k.kirovich@gmail.com>
2022-07-06 17:43:26 +0300

    (finally) Api call

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@ import Header from "./components/shared/header/Header";
 import CurrentWeather from "./components/common/currentWeather/CurrentWeather.jsx";
 import CurrentWeatherInfo from "./components/common/CurrentWeatherInfo/CurrentWeatherInfo";
 import Cards from "./components/common/Cards/Cards";
+import { useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=7ec8d62371de48d48be165112220507&q=${value}&days=10&aqi=no&alerts=no`
+      )
+        .then((data) => data.json())
+        .then((res) => setCity(res))
+        .then(() => console.log(city));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="App">
-      <Header />
-      <div className='weatherBlock'>
+      <Header onChange={handleChange} handleSubmit={handleSubmit} />
+      <div className="weatherBlock">
         <CurrentWeather />
         <CurrentWeatherInfo />
       </div>
-      <Cards/>
+      <Cards />
     </div>
   );
 }
--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,41 +1,19 @@
 import s from "./Header.module.scss";
 import Logo from "../../../assets/SVGs/Logo.jsx";
 import Theme from "../../../assets/SVGs/Theme.jsx";
-// import { useState } from "react";
 
-const Header = () => {
-  //   const [selectList, setSelectList] = useState([]);
-  //   setSelectList(
-  //     <option value="grapefruit">Grapefruit</option>,
-  //     <option value="lime">Lime</option>,
-  //     <option value="coconut">Coconut</option>,
-  //     <option value="mango">Mango</option>
-  //   );
-
-  //   const fillSelectList = () => {
-  //     setSelectList(
-  //       selectList.push(<option value="grapefruit">Grapefruit</option>)
-  //     );
-  //   };
-  //   fillSelectList();
-
-  //   console.log(selectList);
+const Header = (props) => {
 
   return (
     <div className={s.header}>
       <div className={s.header_side}>
-        <Logo/>
+        <Logo />
         <h1 className={s.header_side_right}>react weather</h1>
       </div>
       <div className={s.header_side}>
         <Theme />
-        <form className={s.header_side_right} action="">
-          <select name="cities" id="cities" >
-            <option value="grapefruit">Grapefruit</option>,
-            <option value="lime">Lime</option>,
-            <option value="coconut">Coconut</option>,
-            <option value="mango">Mango</option>
-          </select>
+        <form className={s.header_side_right} action="" onSubmit={props.handleSubmit}>
+          <input type="text" onChange={props.onChange}/>
         </form>
       </div>
     </div>
