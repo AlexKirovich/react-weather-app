a2b645421319c7651def5a144d4f73cebdb87ef2
AlexKirovich <alex.k.kirovich@gmail.com>
2022-07-07 20:14:14 +0300

    improved imports

---
+++ src/Imports.jsx
@@ -0,0 +1,7 @@
+export {default as Logo} from './assets/SVGs/Logo.jsx'
+export {default as Theme} from './assets/SVGs/Theme.jsx'
+export {default as Sun} from './assets/SVGs/Sun.jsx'
+export {default as Temp} from './assets/SVGs/Temp.jsx'
+export {default as Humidity} from './assets/SVGs/Humidity.jsx'
+export {default as Wind} from './assets/SVGs/Wind.jsx'
+export {default as Precipitation} from './assets/SVGs/Precipitation.jsx'
