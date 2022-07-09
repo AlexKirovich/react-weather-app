5b304cc50a2f660329096518d152db77b6c6625f
AlexKirovich <alex.k.kirovich@gmail.com>
2022-07-09 19:03:51 +0300

    More adaptive things

--- src/components/common/currentWeather/CurrentWeather.jsx
+++ src/components/common/currentWeather/CurrentWeather.jsx
@@ -3,11 +3,12 @@ import { Sun } from "../../../Imports";
 
 const CurrentWeather = (props) => {
   const TIME = props.time.split(" ")[1];
+  const temp = Math.round(props.temp);
   return (
     <div className={s.current_weather}>
       <div className={s.temperature_block}>
         <div className={s.temperature}>
-          <h2>{props.temp}°</h2>
+          <h2>{temp}°</h2>
           <p>Today</p>
         </div>
         <Sun />
